0e471fdb8eee3d5c8907764f0440c9c7ec48e1e7
Daichi Yamauchi <57288041+daichi-yamauchi@users.noreply.github.com>
2024-10-29 21:54:53 +0900

    ✨ データベースを追加する

--- src/index.tsx
+++ src/index.tsx
@@ -6,10 +6,13 @@ const app = new Hono<{ Bindings: Env }>();
 app.get('*', renderer);
 
 app.get('/', async (c) => {
+	const { results } = await c.env.DB.prepare('SELECT "Hello, World!" AS title').run();
+
 	return c.render(
 		<div>
 			<h1>匿名掲示板</h1>
 			<p>ようこそ！</p>
+			{results[0]?.title}
 		</div>
 	);
 });
